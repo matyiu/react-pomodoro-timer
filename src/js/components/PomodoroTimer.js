74fcf2f245eb5e67a3f42511f178854e09edfb87
Jefferson Valle <valleespinozajefferson@gmail.com>
2021-04-15 14:15:07 +0200

    Added configuration view

--- src/js/components/PomodoroTimer.js
+++ src/js/components/PomodoroTimer.js
@@ -9,10 +9,47 @@ const ucfirst = (([char, ...rest]) => {
     return [char.toUpperCase(), ...rest].join('');
 });
 
+const PomodoroView = ({position, prettyTimer, cycle, onPlay, onNext, onView}) => {
+    return (
+        <>
+            <h3>{ucfirst(cycle)}</h3>
+            <button className="timer-view" onClick={onView}><i className="fas fa-cog"></i></button>
+            <p className="timer-round">{position + 1}/{LONGBREAKCYCLE}</p>
+            <div className="timer">
+                <span className="timer-text">{prettyTimer}</span>
+            </div>
+            <div className="timer-controls">
+                <button onClick={onPlay} className="timer-pause">Start</button>
+                <button className="timer-next" onClick={onNext}>
+                    Next: {cycle === 'work' ? 'Break' : 'Work'} 
+                </button>
+            </div>
+        </>
+    );
+}
+
+const ConfigurationView = ({onAutomaticBreak, automaticBreak, onView}) => {
+    return (
+        <>
+            <h3>Configuration</h3>
+            <button className="timer-view" onClick={onView}><i className="fas fa-times"></i></button>
+            <div className="timer-form">
+                <div className="timer-row">
+                    <label htmlFor="automatic-breaks">Automatic Breaks?</label>
+                    <input type="checkbox" id="automatic-breaks" 
+                        onChange={onAutomaticBreak} value={automaticBreak}/>
+                </div>
+            </div>
+        </>
+    );
+}
+
 const PomodoroTimer = () => {
     const [ time, setTime ] = useState(WORKTIME);
     const [ cycle, setCycle ] = useState('work');
     const [ position, setPosition ] = useState(0);
+    const [ view, setView ] = useState(0);
+    const [ automaticBreak, setAutomaticBreak ] = useState(false);
     const timer = useRef(null);
 
     const setBreak = () => {
@@ -78,6 +115,9 @@ const PomodoroTimer = () => {
         }
     }
 
+    const handleAutomaticBreak = () => setAutomaticBreak(!automaticBreak);
+    const handleConfigurationView = () => setView(1);
+    const handlePomodoroView = () => setView(0);
     
     const minutes = Math.trunc(time / 60);
     const seconds = time - (minutes * 60);
@@ -89,18 +129,22 @@ const PomodoroTimer = () => {
 
     return (
         <div className="pomodoro-timer">
-            <h3>{ucfirst(cycle)}</h3>
-            <p className="timer-round">{position + 1}/{LONGBREAKCYCLE}</p>
-            <div className="timer">
-                <span className="timer-text">{prettyTimer}</span>
-            </div>
-            <div className="timer-controls">
-                <button onClick={handlePlayTimer} className="timer-pause">Start</button>
-                <button className="timer-next" onClick={handleNext}>
-                    Next: {cycle === 'work' ? 'Break' : 'Work'} 
-                    <i class="fas fa-arrow-right"></i>
-                </button>
-            </div>
+            {
+                view === 0 ? 
+                <PomodoroView 
+                    cycle={cycle} 
+                    prettyTimer={prettyTimer}
+                    position={position}
+                    onPlay={handlePlayTimer}
+                    onNext={handleNext}
+                    onView={handleConfigurationView}
+                /> :
+                <ConfigurationView 
+                    automaticBreak={automaticBreak} 
+                    onAutomaticBreak={handleAutomaticBreak} 
+                    onView={handlePomodoroView}
+                />
+            }
         </div>
     );
 }
